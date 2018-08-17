ccd3dc29e37c8662b863ecac58407679c336f391
Arslan <arslanaziz@appsgenii.eu>
2018-08-17 15:23:28 +0500

    Magefan - Original version
    Without Editing

---
+++ Helper/Data.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © 2017 Ihor Vansach (ihor@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Magefan\Blog\Helper;
+
+use Magento\Framework\App\Action\Action;
+
+/**
+ * Magefan Blog Helper
+ */
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    public function getConfig($config_path)
+    {
+        return $this->scopeConfig->getValue(
+            $config_path,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+}
